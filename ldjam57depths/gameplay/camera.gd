a95416f89939b8efc0e0dddcbd87acc1ba6a4e43
Aleksi Martikainen <aleksi.martikainen@gofore.com>
2025-04-06 15:23:24 +0300

    Bugikorjauksia

--- ldjam57depths/gameplay/camera.gd
+++ ldjam57depths/gameplay/camera.gd
@@ -29,14 +29,17 @@ func _unhandled_input(event):
 			zoom *= 1.0 - zoom_speed
 			zoom.x = clamp(zoom.x, min_zoom, max_zoom)
 			zoom.y = clamp(zoom.y, min_zoom, max_zoom)
+			var new_focus = map_range(zoom.x, min_zoom, max_zoom, 0, 100)
+			SIGNAL_BUS.focus_changed.emit(new_focus)
 		elif event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
 			zoom *= 1.0 + zoom_speed
 			zoom.x = clamp(zoom.x, min_zoom, max_zoom)
 			zoom.y = clamp(zoom.y, min_zoom, max_zoom)
+			var new_focus = map_range(zoom.x, min_zoom, max_zoom, 0, 100)
+			SIGNAL_BUS.focus_changed.emit(new_focus)
 		elif event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 			SIGNAL_BUS.picture_taken.emit()
-	var new_focus = map_range(zoom.x, min_zoom, max_zoom, 0, 100)
-	SIGNAL_BUS.focus_changed.emit(new_focus)
+
 
 
 func map_range(value: float, low1: float, high1: float, low2: float, high2: float) -> float:
