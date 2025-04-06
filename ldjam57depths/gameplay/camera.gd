9c475f53c1c3deb5934442ae3d1eb9cc3a5d6889
Tatu Heinämäki <tatu.heinamaki@gmail.com>
2025-04-06 21:32:43 +0900

    Added new inputs into camera code.

--- ldjam57depths/gameplay/camera.gd
+++ ldjam57depths/gameplay/camera.gd
@@ -24,22 +24,16 @@ func _process(_delta):
 
 
 func _unhandled_input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
-			zoom *= 1.0 - zoom_speed
-			zoom.x = clamp(zoom.x, min_zoom, max_zoom)
-			zoom.y = clamp(zoom.y, min_zoom, max_zoom)
-			var new_focus = map_range(zoom.x, min_zoom, max_zoom, 0, 100)
-			SIGNAL_BUS.focus_changed.emit(new_focus)
-		elif event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
-			zoom *= 1.0 + zoom_speed
-			zoom.x = clamp(zoom.x, min_zoom, max_zoom)
-			zoom.y = clamp(zoom.y, min_zoom, max_zoom)
-			var new_focus = map_range(zoom.x, min_zoom, max_zoom, 0, 100)
-			SIGNAL_BUS.focus_changed.emit(new_focus)
-		elif event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-			SIGNAL_BUS.picture_taken.emit()
-
+	if event.is_action_pressed("zoom_out"):
+		zoom *= 1.0 - zoom_speed
+		zoom.x = clamp(zoom.x, min_zoom, max_zoom)
+		zoom.y = clamp(zoom.y, min_zoom, max_zoom)
+	elif event.is_action_pressed("zoom_in"):
+		zoom *= 1.0 + zoom_speed
+		zoom.x = clamp(zoom.x, min_zoom, max_zoom)
+		zoom.y = clamp(zoom.y, min_zoom, max_zoom)
+	elif event.is_action_pressed("shutter"):
+		SIGNAL_BUS.picture_taken.emit()
 
 
 func map_range(value: float, low1: float, high1: float, low2: float, high2: float) -> float:
